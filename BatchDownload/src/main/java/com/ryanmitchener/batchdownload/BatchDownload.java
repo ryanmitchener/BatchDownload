bf22050573be0a4b7899637394c3afeb4707fd19
Ryan Mitchener <RyanMitchener@gmail.com>
2014-07-14 20:19:11 -0400

    - Initial Commit

---
+++ BatchDownload/src/main/java/com/ryanmitchener/batchdownload/BatchDownload.java
@@ -0,0 +1,446 @@
+package com.ryanmitchener.batchdownload;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Copyright (c) 2014 Ryan Mitchener
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+// NOTES: Requires Internet permission to use <uses-permission android:name="android.permission.INTERNET" />
+
+// Class for downloading files concurrently on multiple threads
+public class BatchDownload {
+    // Misc variables
+    private long total_downloaded = 0;
+    private long total_bytes = 0;
+    private int error_count = 0;
+    private static LocalBroadcastManager bm;
+
+    // File/folder variables
+    private static String CACHE_PATH;
+    private static String DEFAULT_PATH;
+    private static File cache;
+
+    // Thread pool/work queue variables
+    private final BlockingQueue<Runnable> downloadWorkQueue;
+    private final BlockingQueue<Runnable> sizeWorkQueue;
+    private DownloaderThreadPoolExecutor downloadThreadPool;
+    private SizeCalculatorThreadPoolExecutor sizeThreadPool;
+    private final Object byteCountLock = new Object();
+    private final Object sizeCalculatorLock = new Object();
+    private final int CORE_POOL_SIZE = 4;
+    private final int MAX_POOL_SIZE = 5;
+    private final int KEEP_ALIVE_TIME = 10;
+    private boolean sizeCalculated = false;
+
+    // Broadcast Intent Actions
+    public final static String ACTION_CALCULATING = "com.BatchDownload.ACTION_CALCULATING";
+    public final static String ACTION_PROGRESS = "com.BatchDownload.ACTION_PROGRESS";
+    public final static String ACTION_ERROR = "com.BatchDownload.ACTION_ERROR";
+    public final static String ACTION_FILE_DOWNLOADED = "com.BatchDownload.ACTION_FILE_DOWNLOADED";
+    public final static String ACTION_COMPLETE = "com.BatchDownload.ACTION_COMPLETE";
+    public final static String ACTION_CANCELLED = "com.BatchDownload.ACTION_CANCELLED";
+
+    // Broadcast Intent extras
+    public final static String EXTRA_BYTES_DOWNLOADED = "com.BatchDownload.EXTRA_TOTAL_DOWNLOADED";
+    public final static String EXTRA_TOTAL_BYTES = "com.BatchDownload.EXTRA_TOTAL_BYTES";
+    public final static String EXTRA_ERROR_COUNT = "com.BatchDownload.EXTRA_ERROR_COUNT";
+    public final static String EXTRA_FILENAME = "com.BatchDownload.EXTRA_FILENAME";  // Only available in ACTION_FILE_DOWNLOADED
+    public final static String EXTRA_FILEPATH = "com.BatchDownload.EXTRA_FILEPATH";  // Only available in ACTION_FILE_DOWNLOADED
+    public final static String EXTRA_ERROR_URL = "com.BatchDownload.EXTRA_ERROR_URL";  // Only available in ACTION_ERROR
+
+    // Create Singleton pattern
+    private static BatchDownload sInstance = new BatchDownload();
+
+
+    /**
+     * Constructor
+     * ---------------------------------------------------------------------------------------------
+     */
+
+    // Constructor width list of DownloadRequests
+    private BatchDownload() {
+        this.downloadWorkQueue = new LinkedBlockingQueue<Runnable>();
+        this.downloadThreadPool = new DownloaderThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, downloadWorkQueue);
+        this.sizeWorkQueue = new LinkedBlockingQueue<Runnable>();
+        this.sizeThreadPool = new SizeCalculatorThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, sizeWorkQueue);
+    }
+
+
+    // Singleton pattern instance
+    public static BatchDownload getInstance(Context context) {
+        bm = LocalBroadcastManager.getInstance(context);
+
+        // Create the cache directory
+        DEFAULT_PATH = context.getFilesDir().getAbsolutePath() + "/";
+        CACHE_PATH = DEFAULT_PATH + "com.batchdownload.cache/";
+        cache = new File(CACHE_PATH);
+        if (!cache.exists()) {
+            cache.mkdir();
+        }
+
+        // Return singleton
+        return sInstance;
+    }
+
+
+    /**
+     * Misc Methods
+     * ---------------------------------------------------------------------------------------------
+     */
+
+    // Add a request
+    public void add(Request request) {
+        // Send calculating size broadcast
+        if (!isRunning()) {
+            sendBroadcast(ACTION_CALCULATING, null);
+        }
+
+        // Add to thread pool
+        downloadThreadPool.execute(new DownloaderTask(request));
+        sizeThreadPool.execute(new SizeCalculateTask(request));
+    }
+
+
+    // Add a list of requests
+    public void add(ArrayList<Request> requests) {
+        // Send calculating size broadcast
+        if (!isRunning()) {
+            sendBroadcast(ACTION_CALCULATING, null);
+        }
+
+        // Populate thread pool
+        for (Request request : requests) {
+            downloadThreadPool.execute(new DownloaderTask(request));
+            sizeThreadPool.execute(new SizeCalculateTask(request));
+        }
+    }
+
+
+    // Cancel Method
+    public void cancel() {
+        downloadThreadPool.shutdownNow();
+        sizeThreadPool.shutdownNow();
+    }
+
+
+    // Reset variables
+    private void resetVars() {
+        sizeCalculated = false;
+        error_count = 0;
+        synchronized (byteCountLock) {
+            total_downloaded = 0;
+        }
+        synchronized (sizeCalculatorLock) {
+            total_bytes = 0;
+        }
+
+        // Remove the cache folder
+        File[] cacheFiles = cache.listFiles();
+        for (File file : cacheFiles) {
+            file.delete();
+        }
+    }
+
+
+    // Checks if the downloader is running or not
+    public boolean isRunning() {
+        if (downloadThreadPool.getActiveCount() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+    // Sends broadcast
+    private void sendBroadcast(String type, Bundle extras) {
+        if (extras == null) {
+            extras = new Bundle();
+        }
+        extras.putLong(EXTRA_BYTES_DOWNLOADED, total_downloaded);
+        extras.putLong(EXTRA_TOTAL_BYTES, total_bytes);
+        extras.putInt(EXTRA_ERROR_COUNT, error_count);
+        Intent intent = new Intent(type);
+        intent.putExtras(extras);
+        bm.sendBroadcast(intent);
+    }
+
+
+    /**
+     * Downloader
+     * ---------------------------------------------------------------------------------------------
+     */
+
+    // Download task for downloading urls
+    private class DownloaderTask implements Runnable {
+        private Request request;
+
+        DownloaderTask(Request request) {
+            this.request = request;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Connect to the URL with a GET request this time
+                HttpURLConnection con = (HttpURLConnection) new URL(request.url).openConnection();
+                if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    con.disconnect();
+                    throw new BadHTTPResponseException();
+                }
+
+                // Get the filename
+                String filename = Uri.decode(request.url);
+                filename = filename.substring(filename.lastIndexOf("/") + 1);
+                if (request.filename != null) {
+                    String extension = filename.substring(filename.lastIndexOf("."));
+                    filename = request.filename + extension;
+                }
+
+                // Create directory and file
+                InputStream is = new BufferedInputStream(con.getInputStream());
+                File file = new File(cache, filename);
+
+                // Write data to file
+                OutputStream os = new FileOutputStream(file);
+                byte[] buffer = (is.available() > 4000) ? new byte[is.available()] : new byte[4000];
+                Integer bytesRead;
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    // Cancel the runnable if the thread is interrupted by shutdownNow()
+                    if (Thread.currentThread().isInterrupted()) {
+                        is.close();
+                        os.close();
+                        con.disconnect();
+                        return;
+                    }
+
+                    synchronized (byteCountLock) {
+                        total_downloaded += bytesRead;
+                    }
+                    os.write(buffer, 0, bytesRead);
+
+                    // Send progress update if the size is done calculating
+                    if (sizeCalculated) {
+                        sendBroadcast(ACTION_PROGRESS, null);
+                    }
+                }
+
+                // Close streams
+                is.close();
+                os.close();
+                con.disconnect();
+
+                // Move file to specified location
+                File movedFile;
+                if (request.downloadFolder != null) {
+                    if (!request.downloadFolder.isDirectory()) {
+                        throw new NotADirectoryException();
+                    }
+                    movedFile = new File(request.downloadFolder, filename);
+                } else {
+                    movedFile = new File(DEFAULT_PATH, filename);
+                }
+
+                // Attempt to move the file
+                if (!file.renameTo(movedFile)) {
+                    throw new FailedFileWriteException();
+                }
+
+                // Send FILE_DOWNLOADED broadcast
+                Bundle bundle = new Bundle();
+                bundle.putString(EXTRA_FILENAME, filename);
+                bundle.putString(EXTRA_FILEPATH, movedFile.getAbsolutePath());
+                sendBroadcast(ACTION_FILE_DOWNLOADED, bundle);
+            } catch (Exception e) {
+                error_count++;
+                Bundle bundle = new Bundle();
+                bundle.putString(EXTRA_ERROR_URL, request.url);
+                sendBroadcast(ACTION_ERROR, bundle);
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    // Extension of ThreadPoolExecutor for download tasks
+    private class DownloaderThreadPoolExecutor extends java.util.concurrent.ThreadPoolExecutor {
+        public DownloaderThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        }
+
+        @Override
+        protected void afterExecute(Runnable r, Throwable t) {
+            super.afterExecute(r, t);
+
+            // If current thread is the last in the pool
+            if (downloadThreadPool.getActiveCount() == 1) {
+                sendBroadcast(ACTION_COMPLETE, null);
+
+                // Reset variables
+                resetVars();
+            }
+        }
+
+        @Override
+        protected void terminated() {
+            super.terminated();
+            downloadThreadPool = new DownloaderThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, downloadWorkQueue);
+            sendBroadcast(ACTION_CANCELLED, null);
+            resetVars();
+        }
+    }
+
+
+    /**
+     * Size Calculator
+     * ---------------------------------------------------------------------------------------------
+     */
+
+    // Runnable for calculating the total size of all requests
+    private class SizeCalculateTask implements Runnable {
+        private String url;
+
+        SizeCalculateTask(Request request) {
+            this.url = request.url;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Cancel the runnable if the thread is interrupted by shutdownNow()
+                if (Thread.currentThread().isInterrupted()) {
+                    return;
+                }
+                HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+                con.setRequestMethod("HEAD");
+                if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    synchronized (sizeCalculatorLock) {
+                        total_bytes += con.getContentLength();
+                    }
+                }
+                con.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    // Extension of ThreadPoolExecutor for size request tasks
+    private class SizeCalculatorThreadPoolExecutor extends ThreadPoolExecutor {
+        public SizeCalculatorThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        }
+
+        @Override
+        protected void afterExecute(Runnable r, Throwable t) {
+            super.afterExecute(r, t);
+
+            // If current thread is the last in the pool
+            if (sizeThreadPool.getActiveCount() == 1) {
+                sizeCalculated = true;
+            }
+        }
+
+        @Override
+        protected void terminated() {
+            super.terminated();
+            sizeThreadPool = new SizeCalculatorThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, sizeWorkQueue);
+            resetVars();
+        }
+    }
+
+
+    /**
+     * Container Classes
+     * ---------------------------------------------------------------------------------------------
+     */
+
+    public static class Request {
+        private String url = null;
+        private String filename = null;
+        private File downloadFolder = null;
+
+        public Request(String url) {
+            this.url = url;
+        }
+
+        public Request(String url, String filename) {
+            this.url = url;
+            this.filename = filename;
+        }
+
+        public Request(String url, String filename, File downloadFolder) {
+            this.url = url;
+            this.filename = filename;
+            this.downloadFolder = downloadFolder;
+        }
+    }
+
+
+    /**
+     * Exception Classes
+     * ---------------------------------------------------------------------------------------------
+     */
+
+    // Exception for the specified download folder not being a directory
+    private class NotADirectoryException extends Exception {
+        @Override
+        public String getMessage() {
+            return "The download folder specified is not a directory.";
+        }
+    }
+
+
+    // Exception for the specified download folder not being a directory
+    private class BadHTTPResponseException extends Exception {
+        @Override
+        public String getMessage() {
+            return "The URL has replied with a bad response.";
+        }
+    }
+
+
+    // Exception for not being able to move download to download folder
+    private class FailedFileWriteException extends Exception {
+        @Override
+        public String getMessage() {
+            return "The download failed to be written to the requested folder.";
+        }
+    }
+}
---
+++ app/src/main/java/com/ryanmitchener/batchdownloadtestbed/ActivityMain.java
@@ -0,0 +1,117 @@
+package com.ryanmitchener.batchdownloadtestbed;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ryanmitchener.batchdownload.BatchDownload;
+import com.ryanmitchener.batchdownload.BatchDownload.Request;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class ActivityMain extends Activity {
+    BatchDownload bd;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Test request download folder
+        File testFolder = new File(this.getFilesDir() + "/test/");
+        testFolder.mkdir();
+
+        final ArrayList<Request> requests = new ArrayList<BatchDownload.Request>();
+        requests.add(new Request("http://info.sonicretro.org/images/9/9f/ASR_Big.png", "TEST", testFolder));
+        requests.add(new Request("https://robertsspaceindustries.com/media/qz07ylp60wayer/source/JumpPoint_02-06_Jun_14_The-Next-Great-Jump-Point.pdf"));
+        requests.add(new Request("http://upload.wikimedia.org/wikipedia/commons/8/8b/Webdings-big.png"));
+        requests.add(new Request("http://www.cert.at/static/downloads/certatlogo/logo_big_whitebackground.jpg"));
+        requests.add(new Request("http://img4.wikia.nocookie.net/__cb20120119153317/fantendo/images/7/7a/MP9_Big_Bob-Omb.png"));
+        requests.add(new Request("http://img4.wikia.nocookie.net/__cb20130118022025/ben10/images/6/6d/Big_Chill_Save_Last_Dance_1.PNG"));
+        requests.add(new Request("http://img1.wikia.nocookie.net/__cb20120922200332/ben10/images/8/87/Ultimate_Big_Chill_UA_4.PNG"));
+        requests.add(new Request("http://vnmedia.ign.com/witchervault.ign.com/wiki/7/79/GhortaruisLG.png"));
+        requests.add(new Request("https://robertsspaceindustries.com/media/q7i1qzak8bo1fr/source/JumpPoint_02-05_May_14_Racing-To-Get-Done-1.pdf"));
+        requests.add(new Request("https://robertsspaceindustries.com/media/hynpxrt7s225tr/source/JumpPoint_02-04_Apr_14_Heads-Up.pdf"));
+
+        IntentFilter broadcastFilter = new IntentFilter();
+        broadcastFilter.addAction(BatchDownload.ACTION_CALCULATING);
+        broadcastFilter.addAction(BatchDownload.ACTION_ERROR);
+        broadcastFilter.addAction(BatchDownload.ACTION_PROGRESS);
+        broadcastFilter.addAction(BatchDownload.ACTION_FILE_DOWNLOADED);
+        broadcastFilter.addAction(BatchDownload.ACTION_COMPLETE);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new Callback(), broadcastFilter);
+
+        bd = BatchDownload.getInstance(this);
+//        bd.add(requests);
+
+        findViewById(R.id.add_url).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                System.out.println("" + bd.isRunning());
+                bd.add(requests.get(0));
+                requests.remove(0);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        bd.cancel();
+    }
+
+
+    // Callback Receiver
+    private class Callback extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            TextView text = (TextView) findViewById(R.id.message);
+            if (intent.getAction().equals(BatchDownload.ACTION_CALCULATING)) {
+                text.setText("Calculating size...");
+                System.out.println("Calculating...");
+            } else if (intent.getAction().equals(BatchDownload.ACTION_ERROR)) {
+                System.out.println("Error: " + intent.getStringExtra(BatchDownload.EXTRA_ERROR_URL));
+            } else if (intent.getAction().equals(BatchDownload.ACTION_PROGRESS)) {
+                Bundle extras = intent.getExtras();
+                String errors = "" + extras.getInt(BatchDownload.EXTRA_ERROR_COUNT);
+//                int percent = Math.round(((float) extras.getLong(BatchDownload.EXTRA_BYTES_DOWNLOADED, 0) / extras.getLong(BatchDownload.EXTRA_TOTAL_SIZE, 0)) * 100);
+                int percent = (int) ((extras.getLong(BatchDownload.EXTRA_BYTES_DOWNLOADED, 0) * 100) / extras.getLong(BatchDownload.EXTRA_TOTAL_BYTES, 0));
+                String kilobytes = (extras.getLong(BatchDownload.EXTRA_TOTAL_BYTES) / 1000) + "KB";
+                text.setText("" + percent + "%: " + kilobytes);
+            } else if (intent.getAction().equals(BatchDownload.ACTION_FILE_DOWNLOADED)) {
+                System.out.println(intent.getStringExtra(BatchDownload.EXTRA_FILENAME) + " : " + intent.getStringExtra(BatchDownload.EXTRA_FILEPATH));
+            } else if (intent.getAction().equals(BatchDownload.ACTION_COMPLETE)) {
+                text.setText("Download Complete. " + intent.getIntExtra(BatchDownload.EXTRA_ERROR_COUNT, 0) + " errors.");
+            }
+        }
+    }
+}
